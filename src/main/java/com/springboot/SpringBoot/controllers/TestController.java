e02bfc4f195339c6cca17681c19fe6d1a2b95777
PAARTH2608 <jain.paarth2608@gmail.com>
2023-10-27 01:06:08 +0530

    add:env-vars

--- src/main/java/com/springboot/SpringBoot/controllers/TestController.java
+++ src/main/java/com/springboot/SpringBoot/controllers/TestController.java
@@ -1,13 +1,15 @@
 package com.springboot.SpringBoot.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    @Value("${test.message}")
+    private String testMessage;
     @GetMapping("/")
     public String test() {
-        return "API is up and running on port 8000 !";
+        return testMessage;
     }
 }
